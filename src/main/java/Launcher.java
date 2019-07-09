ef3167fbc98ef20466fcb76f0668f7bccc903f73
NaagAlgates <nagharaj.cse@gmail.com>
2019-07-09 10:11:30 +1000

    Connectable Obervable example

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,6 +1,7 @@
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observables.ConnectableObservable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,11 +13,13 @@ class Launcher {
                 Arrays.asList("Alpha", "Beta", "Gamma", "Delta",
                         "Epsilon","sdfsdfsd","rewrwer","32432423","asfdasdas","asdasdasd","sdfsdfsdfsdf","sdfsdfsdfsdfsdfsdfsdf","sdfsdf");
 
-        Observable<String> myStrings = Observable.fromIterable(items);
+        ConnectableObservable<String> myStrings = Observable.fromIterable(items).publish();
 
         myStrings.map(String::length).filter(i->i>5).subscribe((System.out::println),
                 Throwable::printStackTrace);
         myStrings.subscribe(s->System.out.println(s));
+
+        myStrings.connect();
 
         //myStrings.subscribe(System.out::println);
         //myStrings.map(String::length).subscribe(System.out::println);;
